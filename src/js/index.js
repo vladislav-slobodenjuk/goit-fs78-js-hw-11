87aab659d4cd0a2885086b7c3fc7386c8d68f476
vladislav-slobodenjuk <vladjun@gmail.com>
2023-05-21 01:39:55 +0300

    createMarkup & updateGallery

--- src/js/index.js
+++ src/js/index.js
@@ -10,22 +10,94 @@ const { formEl, loadMoreBtn, galleryEl } = refs;
 const imgService = new ImgService();
 
 formEl.addEventListener('submit', onSubmit);
+loadMoreBtn.addEventListener('click', onLoadMore);
 // window.addEventListener("scroll", handleScroll);
 
-function onSubmit(e) {
+async function onSubmit(e) {
   e.preventDefault();
   const value = e.target.elements.searchQuery.value.trim();
-  console.log(value);
-
   if (value.length === 0) return console.log('Empty q notification');
 
   imgService.searchQuery = value;
-  // imgService.resetPage();
+  imgService.resetPage();
   clearGallery();
 
-  imgService.getImages();
+  try {
+    const images = await fetchImages();
+    console.log(images);
+    const markup = createMarkup(images);
+    updateGallery(markup);
+  } catch (error) {}
 }
 
 function clearGallery() {
   galleryEl.innerHTML = '';
+}
+
+function updateGallery(markup) {
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+}
+
+async function fetchImages() {
+  try {
+    const { hits } = await imgService.getImages();
+    console.log('file: index.js:47  fetchImages  hits:', hits);
+    if (hits.length === 0) throw new Error('empty result');
+
+    return hits;
+    //
+  } catch (error) {
+    console.log(error);
+    // return console.log('Empty array');
+  }
+}
+
+function createMarkup(items) {
+  return items
+    .map(item => {
+      const {
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = item;
+      return `<div class="col">
+                <div class="card photo-card h-100" ">
+                  <img
+                    src=${webformatURL}
+                    alt=${tags}
+                    class="card-img-top"
+                    loading="lazy"
+                    />
+                  <div class="info card-body">
+                    <p class="info-item card-text">
+                      <b>Likes</b>
+                    </p>
+                    <p class="info-item card-text">
+                      <b>Views</b>
+                    </p>
+                    <p class="info-item card-text">
+                      <b>Comments</b>
+                    </p>
+                    <p class="info-item card-text">
+                      <b>Downloads</b>
+                    </p>
+                  </div>
+                </div>
+              </div>`;
+    })
+    .join('');
+  //
+}
+
+async function onLoadMore() {
+  try {
+    const images = await fetchImages();
+    console.log(images);
+    const markup = createMarkup(images);
+    updateGallery(markup);
+  } catch (error) {}
 }
