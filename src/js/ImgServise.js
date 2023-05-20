270bb6eab3aa36b8ba6b4b2fd4565dc66092517b
vladislav-slobodenjuk <vladjun@gmail.com>
2023-05-20 22:37:43 +0300

    axios defaults & ImgService

--- src/js/ImgServise.js
+++ src/js/ImgServise.js
@@ -1,5 +1,52 @@
 import axios from 'axios';
 
+const BASE_URL = 'https://pixabay.com/api/';
+const params = {
+  key: '24000598-4cbb5e18617bf8e66757f824b',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 40,
+};
+
+axios.defaults.baseURL = BASE_URL;
+axios.defaults.params = params;
+axios.defaults.timeout = 2500;
+
 export class ImgService {
+  #page;
+  #searchQuery;
+  constructor() {
+    this.#page = 1;
+    this.#searchQuery = '';
+  }
   //
+
+  async getImages() {
+    try {
+      const params = { q: this.#searchQuery, page: this.#page };
+      const { data } = await axios.get('', { params });
+      console.log(data);
+      this.incrementPage();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  get searchQuery() {
+    return this.#searchQuery;
+  }
+
+  set searchQuery(newQuery) {
+    this.#searchQuery = newQuery;
+  }
+
+  resetPage() {
+    this.#page = 1;
+  }
+
+  incrementPage() {
+    this.#page += 1;
+  }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,25 @@ const refs = {
 };
 
 const { formEl, loadMoreBtn, galleryEl } = refs;
+const imgService = new ImgService();
+
+formEl.addEventListener('submit', onSubmit);
+// window.addEventListener("scroll", handleScroll);
+
+function onSubmit(e) {
+  e.preventDefault();
+  const value = e.target.elements.searchQuery.value.trim();
+  console.log(value);
+
+  if (value.length === 0) return console.log('Empty q notification');
+
+  imgService.searchQuery = value;
+  // imgService.resetPage();
+  clearGallery();
+
+  imgService.getImages();
+}
+
+function clearGallery() {
+  galleryEl.innerHTML = '';
+}
