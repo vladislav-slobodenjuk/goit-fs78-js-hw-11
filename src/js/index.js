e1b3eb98d05815cba29ce5459b3a2f26e6dd1a4e
vladislav-slobodenjuk <vladjun@gmail.com>
2023-05-20 19:28:19 +0300

    add packeges & loadMoreBtn & bootstrap classes

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import { ImgService } from './ImgServise';
+
+const refs = {
+  formEl: document.getElementById('search-form'),
+  loadMoreBtn: document.querySelector('.load-more'),
+  galleryEl: document.querySelector('.gallery'),
+};
+
+const { formEl, loadMoreBtn, galleryEl } = refs;
